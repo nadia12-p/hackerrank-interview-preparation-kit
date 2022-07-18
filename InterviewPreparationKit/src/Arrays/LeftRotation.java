52c5e7350dcb11c45ec4e65e0c0d49293ba040aa
nadia12-p <nadiapintos98@gmail.com>
2022-07-17 19:35:32 -0500

    rotating an array using modules

---
+++ InterviewPreparationKit/src/Arrays/LeftRotation.java
@@ -0,0 +1,67 @@
+package Arrays;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class ArraysSolution1 {
+
+    /*
+     * Complete the 'rotLeft' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER d
+     */
+
+    public static List<Integer> rotLeft(List<Integer> a, int d) {
+        // Write your code here
+        List<Integer> b = new ArrayList<Integer>(a);
+        for(int i = 0; i < a.size(); i++){
+            int newLocation = (i + (a.size() - d)) % a.size();
+            b.set(newLocation, a.get(i));
+        }
+
+        return b;
+
+    }
+
+}
+
+public class LeftRotation {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("output.txt"));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = ArraysSolution1.rotLeft(a, d);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
