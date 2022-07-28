1f0269a62714332b9dd8579a12a368bec2705c06
nadia12-p <nadiapintos98@gmail.com>
2022-07-28 13:17:45 -0500

    added the count triplets problem, stuck on solution, will work on another problem for now

--- InterviewPreparationKit/src/DirectionariesAndHashmaps/CountTriplets.java
+++ InterviewPreparationKit/src/DirectionariesAndHashmaps/CountTriplets.java
@@ -1,4 +1,46 @@
 package DirectionariesAndHashmaps;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
-public class CountingTriplets {
+public class CountTriplets {
+
+    // Complete the countTriplets function below.
+    static long countTriplets(List<Long> arr, long r) {
+
+        return 0;
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("output.txt"));
+
+        String[] nr = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(nr[0]);
+
+        long r = Long.parseLong(nr[1]);
+
+        List<Long> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Long::parseLong)
+                .collect(toList());
+
+        long ans = countTriplets(arr, r);
+
+        bufferedWriter.write(String.valueOf(ans));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
