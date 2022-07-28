51d1283439e355be0f026d042f22e203b85a881b
nadia12-p <nadiapintos98@gmail.com>
2022-07-28 14:38:20 -0500

    added string manipulation folder

---
+++ InterviewPreparationKit/src/StringManipulation/MakingAnagrams.java
@@ -0,0 +1,51 @@
+package StringManipulation;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class StringSolution1 {
+
+    /*
+     * Complete the 'makeAnagram' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING a
+     *  2. STRING b
+     */
+
+    public static int makeAnagram(String a, String b) {
+        // Write your code here
+
+        return 0;
+
+    }
+
+}
+
+public class MakingAnagrams {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("output.txt"));
+
+        String a = bufferedReader.readLine();
+
+        String b = bufferedReader.readLine();
+
+        int res = StringSolution1.makeAnagram(a, b);
+
+        bufferedWriter.write(String.valueOf(res));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
