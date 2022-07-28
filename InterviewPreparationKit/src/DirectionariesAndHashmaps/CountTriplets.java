95bd08272ae5c0d803d4172c0715dbf8fc9f93bb
nadia12-p <nadiapintos98@gmail.com>
2022-07-28 15:30:36 -0500

    Count Triplets - given a geometric progression find the number of triplets of indices

--- InterviewPreparationKit/src/DirectionariesAndHashmaps/CountTriplets.java
+++ InterviewPreparationKit/src/DirectionariesAndHashmaps/CountTriplets.java
@@ -16,7 +16,25 @@ public class CountTriplets {
     // Complete the countTriplets function below.
     static long countTriplets(List<Long> arr, long r) {
 
-        return 0;
+        HashMap<Long, Long> potential = new HashMap<Long, Long>();
+        HashMap<Long, Long> counter = new HashMap<Long, Long>();
+        long count = 0;
+
+        for(int i = 0; i<arr.size(); i++) {
+            long a = arr.get(i);
+            long key = a/r;
+
+            if (counter.containsKey(key) && (a % r) == 0) {
+                count += counter.get(key);
+            }
+            if (potential.containsKey(key) && (a % r == 0)) {
+                long c = potential.get(key);
+                counter.put(a, counter.getOrDefault(a,0L)+c);
+            }
+            potential.put(a,potential.getOrDefault(a,0L)+1);
+        }
+
+        return count;
 
 
     }
