407cf97c15a3e30eb7b6a104783728e67f8204ed
nadia12-p <nadiapintos98@gmail.com>
2022-07-29 13:43:26 -0500

    Sorting: Comparator - used the Comparator class to sort elements

--- InterviewPreparationKit/src/Sorting/ComparatorSolution.java
+++ InterviewPreparationKit/src/Sorting/ComparatorSolution.java
@@ -1,11 +1,51 @@
 package Sorting;
-import java.io.*;
 import java.util.*;
 
-public class Comparator {
+class Player {
+    String name;
+    int score;
+
+    Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
+
+class Checker implements Comparator<Player> {
+    // complete this method
+    public int compare(Player a, Player b) {
+        if(a.score>b.score) {
+            return -1;
+        }
+        else if (a.score<b.score) {
+            return 1;
+        }
+        else {
+            return a.name.compareTo(b.name);
+        }
+    }
+
+
+}
+
+
+public class ComparatorSolution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for(int i = 0; i < player.length; i++){
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
     }
 }
